466deb7de564fc411aff1659c14228b07633cf63
nm-mutia <nm.mutia02@gmail.com>
2019-07-15 16:17:48 +0700

    rename + up contr

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -24,27 +24,28 @@ class Login extends CI_Controller {
 	}
 
 	function aksi_login(){
-			// $username = $this->input->post('username');
-			// $password = $this->input->post('password');
-			// $where = array(
-			// 	'username' => $username,
-			// 	'password' => md5($password)
-			// 	);
-			// $cek = $this->m_login->cek_login("admin",$where)->num_rows();
-			// if($cek > 0){
-			//
-			// 	$data_session = array(
-			// 		'nama' => $username,
-			// 		'status' => "login"
-			// 		);
-			//
-			// 	$this->session->set_userdata($data_session);
+		// $username = $this->input->post('username');
+		// $password = $this->input->post('password');
+		// $where = array(
+		// 	'username' => $username,
+		// 	'password' => md5($password)
+		// 	);
+		// $cek = $this->m_login->cek_login("admin",$where)->num_rows();
+		// if($cek > 0){
+		//
+		// 	$data_session = array(
+		// 		'nama' => $username,
+		// 		'status' => "login"
+		// 		);
+		//
+		// 	$this->session->set_userdata($data_session);
 
-				// redirect(base_url("Admin_Dashboard"));
+			// redirect(base_url("Admin_Dashboard"));
 
-			// }else{
-			// 	echo "Username dan password salah !";
-			// }
-		}
+		// }else{
+		// 	echo "Username dan password salah !";
+		// }
+		redirect('lihatPo', 'refresh');
+	}
 
 }
