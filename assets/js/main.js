79dca2f06c7782bcd08a19f6bd5c1cb9df4f3ed7
nm-mutia <nm.mutia02@gmail.com>
2019-07-19 08:48:25 +0700

    coba

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,7 @@ jQuery(document).ready(function($) {
 	$(".menu-item-has-children.dropdown").each(function() {
 		$(this).on('click', function() {
 			var $temp_text = $(this).children('.dropdown-toggle').html();
-			$(this).children('.sub-menu').prepend('<li class="subtitle">' + $temp_text + '</li>'); 
+			// $(this).children('.sub-menu').prepend('<li class="subtitle">' + $temp_text + '</li>'); 
 		});
 	});
 
