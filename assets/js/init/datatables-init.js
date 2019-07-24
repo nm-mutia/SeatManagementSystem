48e02dde98203734bbde5d1dc03edb11b5b64547
Rifka Annisa <Geminigirl124@gmail.com>
2019-07-24 08:56:16 +0700

    Update datatables-init.js

--- assets/js/init/datatables-init.js
+++ assets/js/init/datatables-init.js
@@ -169,35 +169,35 @@
 
   // $('#bootstrap-data-table-export').row(':eq(0)', { page: 'current' }).select();
 
-	$('#row-select').DataTable( {
-			initComplete: function () {
-				this.api().columns().every( function () {
-					var column = this;
-					var select = $('<select class="form-control"><option value=""></option></select>')
-						.appendTo( $(column.footer()).empty() )
-						.on( 'change', function () {
-							var val = $.fn.dataTable.util.escapeRegex(
-								$(this).val()
-							);
-
-							column
-								.search( val ? '^'+val+'$' : '', true, false )
-								.draw();
-						} );
-
-					column.data().unique().sort().each( function ( d, j ) {
-						select.append( '<option value="'+d+'">'+d+'</option>' )
-					} );
-				} );
-			}
-		} );
-
-    $("#bootstrap-data-table tbody").click(function (event) {
-        $(table.fnSettings().aoData).each(function () {
-            $(this.nTr).removeClass('active');
-        });
-        $(event.target.parentNode).addClass('active');
-    });
+	// $('#row-select').DataTable( {
+	// 		initComplete: function () {
+	// 			this.api().columns().every( function () {
+	// 				var column = this;
+	// 				var select = $('<select class="form-control"><option value=""></option></select>')
+	// 					.appendTo( $(column.footer()).empty() )
+	// 					.on( 'change', function () {
+	// 						var val = $.fn.dataTable.util.escapeRegex(
+	// 							$(this).val()
+	// 						);
+  //
+	// 						column
+	// 							.search( val ? '^'+val+'$' : '', true, false )
+	// 							.draw();
+	// 					} );
+  //
+	// 				column.data().unique().sort().each( function ( d, j ) {
+	// 					select.append( '<option value="'+d+'">'+d+'</option>' )
+	// 				} );
+	// 			} );
+	// 		}
+	// 	} );
+  //
+  //   $("#bootstrap-data-table tbody").click(function (event) {
+  //       $(table.fnSettings().aoData).each(function () {
+  //           $(this.nTr).removeClass('active');
+  //       });
+  //       $(event.target.parentNode).addClass('active');
+  //   });
 
 
 
